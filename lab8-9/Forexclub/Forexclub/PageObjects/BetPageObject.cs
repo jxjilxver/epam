1893d9a4f5c7f8284b57c46076bcf5db10906699
Slava Murugin <murugin.slava@mail.ru>
2021-12-02 05:07:11 +0300

    lab9

---
+++ lab8-9/Forexclub/Forexclub/PageObjects/BetPageObject.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Forexclub.PageObjects
+{
+    class BetPageObject
+    {
+        private IWebDriver _webDriver;
+
+        private readonly By _investmentAmountInput = By.Id("sumInv");
+        private readonly By _buyButton = By.CssSelector(".a-submit");
+        private readonly By _errorMsg = By.CssSelector(".msg-error");
+
+        public bool IsErrorVisible => _webDriver.FindElement(_errorMsg).Displayed;
+
+        public BetPageObject(IWebDriver webDriver)
+        {
+            _webDriver = webDriver;
+        }
+
+        public LibertexMainPageObject PlaceBet(string amount)
+        {
+            new WebDriverWait(_webDriver, TimeSpan.FromSeconds(5)).Until(ExpectedConditions.ElementIsVisible(_investmentAmountInput));
+            _webDriver.FindElement(_investmentAmountInput).Clear();
+            _webDriver.FindElement(_investmentAmountInput).SendKeys(amount);
+            _webDriver.FindElement(_buyButton).Click();
+
+            return new LibertexMainPageObject(_webDriver);
+        }
+
+    }
+}
